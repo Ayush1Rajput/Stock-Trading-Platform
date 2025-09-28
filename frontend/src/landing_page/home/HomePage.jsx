103b0f819a00d661aaa85e15971db4e34fb4c5c7
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-09-28 23:16:03 +0530

    Create Stats components

--- frontend/src/landing_page/home/HomePage.jsx
+++ frontend/src/landing_page/home/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./Hero";
 import Awards from "./Awards";
+import Stats from "./Stats";
 
 export default function HomePage() {
   return (
     <div>
       <Hero />
       <Awards />
+      <Stats />
     </div>
   );
 }
