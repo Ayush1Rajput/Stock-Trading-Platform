71a287479c229dd33fa5232e7968d1ef6e2006eb
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-09-29 00:55:29 +0530

    Create NotFound component for unwanted url search

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import HomePage from "./landing_page/home/HomePage";
+
 import Navbar from "./landing_page/Navbar.jsx";
 import Footer from "./landing_page/Footer.jsx";
+import NotFound from "./landing_page/NotFound.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,6 +14,7 @@ root.render(
     <Navbar />
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
     <Footer />
   </BrowserRouter>
