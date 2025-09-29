deaed3c3c8de79fff71329f910390950d7b7e72f
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-09-29 22:54:04 +0530

    Create Team page/Component

--- frontend/src/landing_page/about/AboutPage.jsx
+++ frontend/src/landing_page/about/AboutPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Hero";
+import Team from "./Team";
 
 export default function AboutPage() {
   return (
     <div>
       <Hero />
+      <Team />
     </div>
   );
 }
