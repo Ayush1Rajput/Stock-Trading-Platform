96319f76c6e5df45b3b681ddaaca6f5e48a11910
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-09-30 22:57:49 +0530

    Create component of CreateTicket and require it

--- frontend/src/landing_page/support/SupportPage.jsx
+++ frontend/src/landing_page/support/SupportPage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 import Hero from "./Hero";
-// import CreateTicket from "./CreateTicket";
+import CreateTicket from "./CreateTicket";
 
 function SupportPage() {
   return (
     <>
       <Hero />
-      {/* <CreateTicket /> */}
+      <CreateTicket />
     </>
   );
 }
