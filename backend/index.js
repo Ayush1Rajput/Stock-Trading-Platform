ee04e2f101b0c8f8b59651ae344096c10a9870ee
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-10-05 10:28:59 +0530

    Add Positions data to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URL);
 
 const { HoldingsModel } = require("./model/HoldingsModel");
+const { PositionsModel } = require("./model/PositionsModel");
 
 const PORT = process.env.PORT || 5000;
 
@@ -133,6 +134,47 @@ const PORT = process.env.PORT || 5000;
 //     });
 
 //     newHolding.save();
+//   });
+//   res.send("Done!");
+// });
+
+// app.get("/addPositions", async (req, res) => {
+//   let tempPositions = [
+//     {
+//       product: "CNC",
+//       name: "EVEREADY",
+//       qty: 2,
+//       avg: 316.27,
+//       price: 312.35,
+//       net: "+0.58%",
+//       day: "-1.24%",
+//       isLoss: true,
+//     },
+//     {
+//       product: "CNC",
+//       name: "JUBLFOOD",
+//       qty: 1,
+//       avg: 3124.75,
+//       price: 3082.65,
+//       net: "+10.04%",
+//       day: "-1.35%",
+//       isLoss: true,
+//     },
+//   ];
+
+//   tempPositions.forEach((item) => {
+//     let newPosition = new PositionsModel({
+//       product: item.product,
+//       name: item.name,
+//       qty: item.qty,
+//       avg: item.avg,
+//       price: item.price,
+//       net: item.net,
+//       day: item.day,
+//       isLoss: item.isLoss,
+//     });
+
+//     newPosition.save();
 //   });
 //   res.send("Done!");
 // });
