c86cfac3bbc0b0063202d5b33514eeaeb99911e6
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-10-05 10:04:07 +0530

    connect MongoDB and env

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URL);
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   console.log("Server is running on port 5000");
 });
