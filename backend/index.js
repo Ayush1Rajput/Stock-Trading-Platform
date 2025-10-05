bd160344eed06156f01c3fa36891f6c3eabaf7f5
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-10-05 11:18:31 +0530

    Get Holdings data from DB

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,16 @@ const app = express();
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URL);
 
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const { HoldingsModel } = require("./model/HoldingsModel");
 const { PositionsModel } = require("./model/PositionsModel");
 
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 // app.get("/addHoldings", async (req, res) => {
 //   let tempHoldings = [
