294d3dbcc89759fef8ed2f03b2ff717168652ffa
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-10-08 10:03:43 +0530

    Write post route for add newOrder to DB

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ const cors = require("cors");
 
 const { HoldingsModel } = require("./model/HoldingsModel");
 const { PositionsModel } = require("./model/PositionsModel");
+const { OrdersModel } = require("./model/OrdersModel");
 
 const PORT = process.env.PORT || 5000;
 
@@ -193,6 +194,19 @@ app.get("/allHoldings", async (req, res) => {
 app.get("/allPositions", async (req, res) => {
   let allPositions = await PositionsModel.find({});
   res.json(allPositions);
+});
+
+app.post("/newOrder", async (req, res) => {
+  let newOrder = new OrdersModel({
+    name: req.body.name,
+    qty: req.body.qty,
+    price: req.body.price,
+    mode: req.body.mode,
+  });
+
+  newOrder.save();
+
+  res.send("Order saved!");
 });
 
 app.listen(PORT, () => {
