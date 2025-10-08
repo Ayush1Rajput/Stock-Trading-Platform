dd67ca639a702e9659c6e3cbef665c0355172ebd
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-10-08 10:23:29 +0530

    Connect it with backend with help of Axios

--- dashboard/src/components/BuyActionWindow.jsx
+++ dashboard/src/components/BuyActionWindow.jsx
@@ -1,8 +1,25 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
+import axios from "axios";
+
 import GeneralContext from "./GeneralContext";
 
 const BuyActionWindow = ({ uid }) => {
+  const [stockQuantity, setStockQuantity] = useState(1);
+  const [stockPrice, setStockPrice] = useState(0.0);
+
+  const handleBuyClick = () => {
+    axios.post("http://localhost:5000/newOrder", {
+      name: uid,
+      qty: stockQuantity,
+      price: stockPrice,
+      mode: "BUY",
+    });
+
+    GeneralContext.closeBuyWindow();
+  };
+
   const handleCancelClick = () => {
     GeneralContext.closeBuyWindow();
   };
@@ -13,11 +30,24 @@ const BuyActionWindow = ({ uid }) => {
         <div className="inputs">
           <fieldset>
             <legend>Qty.</legend>
-            <input type="number" name="qty" id="qty" />
+            <input
+              type="number"
+              name="qty"
+              id="qty"
+              onChange={(e) => setStockQuantity(e.target.value)}
+              value={stockQuantity}
+            />
           </fieldset>
           <fieldset>
             <legend>Price</legend>
-            <input type="number" name="price" id="price" step="0.05" />
+            <input
+              type="number"
+              name="price"
+              id="price"
+              step="0.05"
+              onChange={(e) => setStockPrice(e.target.value)}
+              value={stockPrice}
+            />
           </fieldset>
         </div>
       </div>
@@ -25,7 +55,9 @@ const BuyActionWindow = ({ uid }) => {
       <div className="buttons">
         <span>Margin required ₹140.65</span>
         <div>
-          <Link className="btn btn-blue">Buy</Link>
+          <Link className="btn btn-blue" onClick={handleBuyClick}>
+            Buy
+          </Link>
           <Link to="" className="btn btn-grey" onClick={handleCancelClick}>
             Cancel
           </Link>
